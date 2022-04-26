50c28137fea53622517a6d3222b65231d51bada4
wancat <wancat@wancat.cc>
2022-04-26 10:25:45 +0800

    Init project

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <header></header>
+    <div class="container">
+      <div><a class="gradient-text" href="/">ISCN Browser</a></div>
+      <div id="tags-list">
+        <h3>Featured Tags</h3>
+        <ul data-bind="tags" data-attr="list"></ul>
+      </div>
+      <div id="block-list">Loading...</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+  css: ['@/assets/css/index.css'],
+}
+</script>
